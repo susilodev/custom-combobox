ba39af75243e304cdeaeb66dcc959758a5fdadb7
Susilloxxxx <susiloabdulmujib@outlook.com>
2025-02-09 04:35:21 +0700

    chore: seteup tonfig

--- src/components/CustomMultipleCombobox.tsx
+++ src/components/CustomMultipleCombobox.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useState, useMemo, useEffect, useRef } from "react";
 import { useCombobox, useMultipleSelection, useSelect } from "downshift";
 import { cn } from "../utils/cn";
