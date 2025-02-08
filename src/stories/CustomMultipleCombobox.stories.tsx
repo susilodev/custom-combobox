e503eb503c967d4b32cfa63bf913fdc1d01f8454
Susilloxxxx <susiloabdulmujib@outlook.com>
2025-02-09 03:30:52 +0700

    feat: adjust combobox

--- src/stories/CustomMultipleCombobox.stories.tsx
+++ src/stories/CustomMultipleCombobox.stories.tsx
@@ -5,8 +5,6 @@ import {
   Option,
 } from "../components/CustomMultipleCombobox"; // Sesuaikan dengan path komponen Anda
 
-import { useArgs } from "@storybook/client-api";
-
 export default {
   title: "Components/CustomMultipleCombobox", // Judul story di Storybook
   component: CustomMultipleCombobox, // Komponen yang akan didokumentasikan
@@ -23,24 +21,27 @@ const Template = (args: MultipleComboboxProps) => (
 );
 
 // Story default
-export const Default = Template.bind({});
-Default.args = {
-  options: [
-    { value: "option-1", label: "Option 1" },
-    { value: "option-with-icon", label: "Option with icon" },
-    { value: "long-long-option-3", label: "Long Long option 3" },
-    { value: "long-long-long-option-4", label: "Long Long Long option 4" },
-    {
-      value: "long-long-long-long-option-5",
-      label: "Long Long Long Long option 5",
-    },
-    {
-      value: "long-long-long-long-long-option-6",
-      label: "Long Long Long  Long option 6",
-    },
-  ],
-  onSelect: (selectedItems: Option[]) =>
-    console.log("Selected Items:", selectedItems),
-  multiple: true,
-  withSearch: true,
+// Story default
+export const Default = {
+  args: {
+    options: [
+      { value: "option-1", label: "Option 1" },
+      { value: "option-with-icon", label: "Option with icon" },
+      { value: "long-long-option-3", label: "Long Long option 3" },
+      { value: "long-long-long-option-4", label: "Long Long Long option 4" },
+      {
+        value: "long-long-long-long-option-5",
+        label: "Long Long Long Long option 5",
+      },
+      {
+        value: "long-long-long-long-long-option-6",
+        label: "Long Long Long  Long option 6",
+      },
+    ],
+    onSelect: (selectedItems: Option[]) =>
+      console.log("Selected Items:", selectedItems),
+    multiple: true,
+    withSearch: true,
+  },
+  render: (args: MultipleComboboxProps) => <Template {...args} />,
 };
